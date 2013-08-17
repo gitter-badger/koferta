b499df46853ba05c2ae270cacddfbf4a458c7d71
Kamil Strzempowicz <konserw@gmail.com>
2013-08-17 15:55:10 +0200

    poczatek implementacji klasy Towar

--- Towar.cpp
+++ Towar.cpp
@@ -3,4 +3,86 @@
 Towar::Towar(QObject *parent) :
     QObject(parent)
 {
+
 }
+
+QString Towar::kod() const
+{
+    return m_kod;
+}
+
+void Towar::setKod(const QString &kod)
+{
+    m_kod = kod;
+}
+
+QString Towar::nazwa() const
+{
+    return m_nazwa;
+}
+
+void Towar::setNazwa(const QString &nazwa)
+{
+    m_nazwa = nazwa;
+}
+
+double Towar::cenaKat() const
+{
+    return m_cenaKat;
+}
+
+void Towar::setCenaKat(double cenaKat)
+{
+    m_cenaKat = cenaKat;
+}
+
+double Towar::rabat() const
+{
+    return m_rabat;
+}
+
+void Towar::setRabat(double rabat)
+{
+    m_rabat = rabat;
+}
+
+double Towar::ilosc() const
+{
+    return m_ilosc;
+}
+
+void Towar::setIlosc(double ilosc)
+{
+    m_ilosc = ilosc;
+}
+
+bool Towar::metr() const
+{
+    return m_metr;
+}
+
+void Towar::setMetr(bool metr)
+{
+    m_metr = metr;
+}
+
+double Towar::cena() const
+{
+    return m_cenaKat * (100 - m_rabat) /100;
+}
+
+double Towar::wartosc() const
+{
+    return m_ilosc * cena();
+}
+
+double Towar::cena(double kurs) const
+{
+    return kurs * m_cenaKat * (100 - m_rabat) /100;
+}
+
+double Towar::wartosc(double kurs) const
+{
+    return m_ilosc * cena(kurs);
+}
+
--- Towar.h
+++ Towar.h
@@ -9,26 +9,42 @@ class Towar : public QObject
 public:
     explicit Towar(QObject *parent = 0);
     
+    QString kod() const;
+    void setKod(const QString &kod);
+
+    QString nazwa() const;
+    void setNazwa(const QString &nazwa);
+
+    double cenaKat() const;
+    void setCenaKat(double cenaKat);
+
+    double rabat() const;
+    void setRabat(double rabat);
+
+    double ilosc() const;
+    void setIlosc(double ilosc);
+
+    bool metr() const;
+    void setMetr(bool metr);
+
 signals:
     void update();
 
 public slots:
-/*
-    void setIlosc(double x);
-    void setRabat(double x);
+    double cena() const;
+    double wartosc() const;
 
-    double cena();
-    double wartosc();
-    double cenaPln();
+    double cena(double kurs) const;
+    double wartosc(double kurs) const;
+
 
 protected:
     QString m_kod;
     QString m_nazwa;
-    double m_cenaKat;
-    double m_rabat;
-    double m_ilosc;
-    bool m_metr;
-*/
+    double  m_cenaKat;
+    double  m_rabat;
+    double  m_ilosc;
+    bool    m_metr;
 };
 
 #endif // TOWAR_H
